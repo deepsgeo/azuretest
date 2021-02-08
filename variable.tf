7e99f2a2c6423ab17094febf6d38648251eb9bc6
Dhanya Devassia <dhanyadeepak10@gmail.com>
2021-02-07 23:15:18 -0500

    variable file

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "location" {
+    type = string
+    description = "Location of the resource group"
+    default = "eastus"
+}    
+variable "vnet_address_space" { 
+    type = list
+    description = "Address space for Virtual Network"
+    default = ["172.16.0.0/16"]
+}    
